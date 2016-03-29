7f31867a4a234cd8f777d256c7e043cd89d7a0ed
private <user@example.com>
2016-03-29 23:30:54 +0100

    Use a before_action to remove duplication in the article controller actions.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,18 @@
 class ArticlesController < ApplicationController
+  before_action :fetch, only: [:edit, :show, :update, :destroy]
+
+  def fetch
+    @article = Article.find(params[:id])
+  end
+
   def index
     @articles = Article.all
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -25,7 +29,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -34,7 +37,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
