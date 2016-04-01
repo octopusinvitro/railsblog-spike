a9d1097e2cc6cddef01842c27247627435c2e88d
private <user@example.com>
2016-04-01 21:36:31 +0100

    Refactor the tests for the article controller.

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -6,64 +6,105 @@ class ArticlesControllerTest < ActionController::TestCase
     @id = articles(:one).id
   end
 
-  test "should get the articles index" do
+  def index
     get :index
+  end
+
+  def edit
+    get :edit, {id: @id}
+  end
+
+  def show
+    get :show, {id: @id}
+  end
+
+  def new_article
+    get :new
+  end
+
+  def create
+    post :create, article: {title: 'A title'}
+  end
+
+  def update
+    put  :update, id: @id, article: {title: 'Yet another title'}
+  end
+
+  def destroy
+    delete :destroy, id: @id
+  end
+
+  def assert_assigns(view_variable)
+    assert_not_nil assigns(view_variable)
+  end
+
+  def assert_renders(template, partial = '')
+    assert_template template
+    if partial.empty?
+      assert_template layout: "layouts/application"
+    else
+      assert_template layout: "layouts/application", partial: partial
+    end
+  end
+
+  test "should get the articles index" do
+    index
     assert_success
-    assert_not_nil assigns(:articles)
+    assert_assigns(:articles)
   end
 
   test "should get an article's page" do
-    get :show, {id: @id}
+    show
     assert_success
-    assert_not_nil assigns(:article)
+    assert_assigns(:article)
   end
 
   test "should not allow to edit, add new, create, update or destroy articles if not authenticated" do
-    get :edit, {id: @id}
+    edit
     assert_no_auth
 
-    get :new
+    new_article
     assert_no_auth
 
-    post :create, article: {title: 'A title'}
+    create
     assert_no_auth
 
-    put  :update, id: @id, article: {title: 'Yet another title'}
+    update
     assert_no_auth
 
-    delete :destroy, id: @id
+    destroy
     assert_no_auth
   end
 
   test "should allow to edit, add new, create, update or destroy articles if authenticated" do
     login
 
-    get  :edit, {id: @id}
+    edit
     assert_success
 
-    get  :new
+    new_article
     assert_success
 
-    post :create, article: {title: 'Another title'}
+    create
     assert_redirect
 
-    put :update, id: @id, article: {title: 'Yet another title'}
+    update
     assert_redirect
 
-    delete :destroy, id: @id
+    destroy
     assert_redirect
   end
 
   test "should post a new article page" do
     login
-    get :new
-    assert_not_nil assigns(:article)
+    new_article
+    assert_assigns(:article)
   end
 
   test "should create article if no errors" do
     login
     assert_difference('Article.count') do
-      post :create, article: {title: 'Some title'}
+      create
     end
     assert_redirected_to article_path(assigns(:article))
   end
@@ -80,7 +121,7 @@ class ArticlesControllerTest < ActionController::TestCase
   test "should update an article if no errors" do
     login
     assert_no_difference('Article.count') do
-      put :update, id: @id, article: {id: @id}
+      update
     end
     assert_redirected_to article_path(assigns(:article))
   end
@@ -96,7 +137,44 @@ class ArticlesControllerTest < ActionController::TestCase
 
   test "deletes an article" do
     login
-    delete :destroy, id: @id
+    destroy
     assert_redirected_to articles_path
   end
+
+  test "renders the index template" do
+    index
+    assert_renders(:index)
+  end
+
+  test "renders the edit template" do
+    login
+    edit
+    assert_renders(:edit, "_form")
+  end
+
+  test "renders the show template" do
+    show
+    assert_renders(:show)
+  end
+
+  test "renders the new template" do
+    login
+    new_article
+    assert_renders(:new, "_form")
+  end
+
+  test "renders the new template if there are errors when creating article" do
+    login
+    bad_title = ''
+    post :create, article: {title: bad_title}
+    assert_renders(:new, "_form")
+  end
+
+  test "renders the edit template if there are errors when updating article" do
+    login
+    bad_title = ''
+    put :update, id: @id, article: {title: bad_title}
+    assert_renders(:edit, "_form")
+  end
+
 end
