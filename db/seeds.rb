e55c306dbb8dc729f8fd62cd437d03bc57b12734
private <user@example.com>
2017-04-17 01:26:37 +0100

    Add some seeds for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+articles = Article.create([
+  {
+    title: "Hey doa",
+    text: "Lorem ipsum"
+  },
+  {
+    title: "Hey",
+    text: "Lorem ipsum"
+  },
+  {
+  title: "new article because yes",
+  text: "Robust flavour, spoon sit pumpkin spice in extra foam steamed. Breve filter seasonal café au lait turkish espresso, café au lait redeye caramelization medium whipped pumpkin spice." +
+"
+
+Turkish, french press ut, organic espresso chicory robust flavour. Ut irish con panna variety arabica medium frappuccino in medium white." +
+"
+
+Saucer body caramelization percolator extra a irish grinder pumpkin spice. Trifecta chicory, blue mountain doppio roast steamed cup skinny single shot espresso."+
+"
+
+Con panna aged con panna cream a redeye cream. Est, espresso arabica single origin id et, sit eu sweet eu that decaffeinated."
+  }
+])
+
+comments = Comment.create([
+  {
+    commenter: "A name",
+    body: "Spit up on light gray carpet instead of adjacent linoleum stand in front of the computer screen, or jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed yet run outside as soon as door open or caticus cuteicus hide when guests come over." +
+"
+
+Sit on the laptop lick sellotape purr for no reason burrow under covers. My left donut is missing, as is my right paw at beetle and eat it before it gets away yet run outside as soon as door open." + 
+"
+
+Sit on the laptop i like big cats and i can not lie howl uncontrollably for no reason mark territory. Meowwww. Eat from dog's food. Russian blue walk on car leaving trail of paw prints on hood and windshield. Play riveting piece on synthesizer keyboard stare at ceiling wake up human for food at 4am, destroy couch as revenge, stare out the window."
+  }
+])
